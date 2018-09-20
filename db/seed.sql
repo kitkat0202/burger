cda88d55cb0d15dd9cf2ad342ac1a7f16a9d1582
KatHe <he.katherine321@gmail.com>
2018-09-20 19:23:53 -0400

    re-try the connection

--- db/seed.sql
+++ db/seed.sql
@@ -2,10 +2,3 @@ INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Big MAC');
 INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
 INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
-
-UPDATE burgers SET burger_name='Veggi Burger' WHERE id=2;
-
-UPDATE burgers SET devoured=true WHERE id=2;
-
-SELECT * FROM burgers;
-
